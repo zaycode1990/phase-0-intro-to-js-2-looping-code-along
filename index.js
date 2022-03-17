aabd7861c0d6cd210037c284bcbe18a7a6185968
Isaiah <ijandicos90@gmail.com>
2022-03-17 19:23:15 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    
+const messages = []
+  
+    for (let i = 0; i < names.length; i++) {
+    
+    const message = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    messages.push(message); }
+
+    return messages
+  }
+
+let tenDigit = 10
+
+  function countDown(tenDigit) {
+    while (tenDigit >= 0) {
+      console.log(tenDigit--)
+      
+    }
+  }
+
+  
+  console.log(countDown(tenDigit))
